8670a4359db4eb06d6c4ed7b3d0157132a97e0e6
Yasmim Brennda <azevedo.brennda@escolar.ifrn.edu.br>
2023-11-28 17:24:47 -0300

    Inserção do nome da primeira integrante do grupo no readme, criação da classe calculator e adição do método add_to_calculation;

--- Atividade 1/scr/code/calculadora.py
+++ Atividade 1/scr/code/calculadora.py
@@ -1,1 +1,24 @@
+import tkinter as tk
 
+class calculator(tk.Tk):
+    
+    def __init__(self):
+        
+        super().__init__()
+
+        self.calculation = ""
+
+        self.text_result = tk.Text(self, height=2, width=16, font=("Arial", 24))
+        self.text_result.grid(columnspan=5)
+
+        self.create_buttons()
+    
+    def add_to_calculation(self, symbol):
+        
+        self.calculation += str(symbol)
+        self.text_result.delete(1.0, "end")
+        self.text_result.insert(1.0, self.calculation)
+
+app = calculator()
+app.geometry("300x275")
+app.mainloop()
--- Readme.md
+++ Readme.md
@@ -1,1 +1,6 @@
+## Grupo de Projeto - Integrantes
 
+**Aluna:** Yasmim Brennda Azevedo Souza <br>
+**Matrícula:** 20201214010004
+
+**Aluna:**
